504669c15c994f52b037053c521095b7f16643ca
Akshay Nayani <166915838+aknayani1865@users.noreply.github.com>
2024-10-05 12:07:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://travel-frontend-28d7.onrender.com", credentials: true }));
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
