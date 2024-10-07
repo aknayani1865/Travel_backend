a6e89bc942a63bbb2c85050ee42927caaa5fc7db
Akshay Nayani <166915838+aknayani1865@users.noreply.github.com>
2024-10-07 15:53:55 +0530

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -7,6 +7,7 @@ import {
 	forgotPassword,
 	resetPassword,
 	checkAuth,
+	updateUserProfile,
 } from "../controllers/auth.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -17,7 +18,7 @@ router.get("/check-auth", verifyToken, checkAuth);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
+router.put('/profile', verifyToken, updateUserProfile);
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
 
